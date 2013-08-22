8d60e01f248a75f7825d5c4a942f391123af1a1d
elemc <me@elemc.name>
2013-08-22 13:08:18 +0400

    Some fixes in views (name etc). Fill seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+arches = Arch.create(
+    [   { name: 'i386' },
+        { name: 'x86_64' },
+    ]
+)
+
+# System !!!
+variants = RepositoryVariant.create(
+    [   { name: 'main', url: '$mirror$/russianfedora/$repos$/releases/$version$/Everything/$arch$/$portion$/' },
+        { name: 'updates', url: '$mirror$/russianfedora/$repos$/updates/$version$/$arch$/$portion$/' },
+        { name: 'updates-tesing', url: '$mirror$/russianfedora/$repos$/updates/testing/$version$/$arch$/$portion$/' },
+        { name: 'development', url: '$mirror$/russianfedora/$repos$/development/$version$/$arch$/$portion$/' },
+        { name: 'build', url: '$mirror$/build/$version$/$portion$/$arch$/' },
+        { name: 'build-development', url: '$mirror$/build/development/$version$/$portion$/$arch$/' },
+    ]
+)
+porions = RepositoryPortion.create(
+    [
+        { name: 'development', url_part: '/os/' },
+        { name: 'main', url_part: '/os/' },
+        { name: 'debug', url_part: '/debug/' },
+        { name: 'source', url_part: '/SRPMS/' },
+    ]
+)
